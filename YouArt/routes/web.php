1d14b41b677451177d01ffa867943852c62d9702
dounia-elg <dounia.elg20@gmail.com>
2025-03-18 11:49:57 +0000

    Configurer les routes pour registretion

--- YouArt/routes/web.php
+++ YouArt/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Registration Routes
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Simple Dashboard Route (for testing)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
