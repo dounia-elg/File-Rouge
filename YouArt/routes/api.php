1db122100dea2990ff112f4b12be8fac3c20c191
dounia-elg <dounia.elg20@gmail.com>
2025-04-05 21:33:17 +0000

    Créez la route de connexion dans routes/api.php

--- YouArt/routes/api.php
+++ YouArt/routes/api.php
@@ -2,5 +2,8 @@
 
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [LoginController::class, 'login']);
