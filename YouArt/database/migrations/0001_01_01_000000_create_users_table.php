1d38caaf0ab2ba8d09c69e1a207dd3877b6bb52f
dounia-elg <dounia.elg20@gmail.com>
2025-04-05 00:18:48 +0000

    MOdifier la migration users

--- YouArt/database/migrations/0001_01_01_000000_create_users_table.php
+++ YouArt/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['artist', 'amateur', 'admin'])->default('amateur');
+            $table->boolean('is_active')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
